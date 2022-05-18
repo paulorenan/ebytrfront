efa3e3d60325d0973575b68ab7af645b783813ac
Paulo Renan <able.paulorenan@gmail.com>
2022-05-18 20:59:23 -0300

    FEAT: header component

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header';
+import { Box } from '@mui/material';
+
+function MainPage() {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  )
+}
+
+export default MainPage;
