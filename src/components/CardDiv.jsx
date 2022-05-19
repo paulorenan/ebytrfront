7993dedf6d89a22db24b9da9062c030f67b7d939
Paulo Renan <able.paulorenan@gmail.com>
2022-05-19 18:34:03 -0300

    FEAT: ordenar Por Titulo

--- src/components/CardDiv.jsx
+++ src/components/CardDiv.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import axios from 'axios';
 import TaskCard from './TaskCard';
 import AddTask from './AddTask';
 
 function CardDiv() {
   const [tasks, setTasks] = useState([]);
+  const [orderAlpha, setOrderAlpha] = useState(false);
 
   useEffect(() => {
     axios.get('https://ebytrback.herokuapp.com/tasks').then(res => {
@@ -17,6 +18,41 @@ function CardDiv() {
     axios.get('https://ebytrback.herokuapp.com/tasks').then(res => {
       setTasks(res.data);
     });
+  };
+
+  const ordenarAlphabet = () => {
+    if (!orderAlpha) {
+      setTasks(tasks.sort((a, b) => a.title.localeCompare(b.title)));
+    } else {
+      setTasks(tasks.sort((a, b) => a.title.localeCompare(b.title)).reverse());
+    }
+    setOrderAlpha(!orderAlpha);
+  };
+
+  const ordenarData = () => {
+    const tasksOrdenadas = tasks.sort((a, b) => {
+      if (a.createdAt < b.createdAt) {
+        return -1;
+      }
+      if (a.createdAt > b.createdAt) {
+        return 1;
+      }
+      return 0;
+    });
+    setTasks(tasksOrdenadas);
+  };
+
+  const ordenarStatus = () => {
+    const tasksOrdenadas = tasks.sort((a, b) => {
+      if (a.status < b.status) {
+        return -1;
+      }
+      if (a.status > b.status) {
+        return 1;
+      }
+      return 0;
+    });
+    setTasks(tasksOrdenadas);
   };
 
   return (
@@ -33,7 +69,36 @@ function CardDiv() {
         marginTop: '0.5rem',
       }}
     >
-      <AddTask fetchTasks={fetchTasks} />
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          width: '100%',
+          maxWidth: '40rem',
+          minWidth: '50px',
+          padding: '1rem',
+          backgroundColor: '#f5f5f5',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <AddTask fetchTasks={fetchTasks} />
+        <Box>
+          <Typography variant="h5" component="div">
+            Ordenar por:
+          </Typography>
+          <Button onClick={ordenarAlphabet}>
+            Ordem Alfabética
+          </Button>
+          <Button onClick={ordenarData}>
+            Data
+          </Button>
+          <Button onClick={ordenarStatus}>
+            Status
+          </Button>
+        </Box>
+      </Box>
       <Box
         sx={{
           display: 'flex',
